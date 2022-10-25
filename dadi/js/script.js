d1082a92533e417313672f5bdf12d1199d15c808
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-10-25 17:38:08 +0200

    console comment

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,3 @@
 console.log('lancia il dado')
+
+let numUser = Math.floor(Math.random() * 6) + 1; 
--- mail/js/script.js
+++ mail/js/script.js
@@ -1,26 +1,28 @@
+console.log('la tua mail')
 const savedEmail = ['prova1@email.com', 'email2@prova.it','emailprova1@prova.it','utente1@mail.com','utente2@mail.it']
 let userEmail = prompt('Inserisci la tua email')
+console.log(savedEmail)
+console.log(userEmail)
 
-console.log('salva la tua mail')
 
 // METODO CON IF
-// if (savedEmail.includes(userEmail)) {
-//     console.log('la mail è già presente')
-//     let userEmail = prompt('inserisci un\'altra email')
-// } else {
-//     savedEmail.push(userEmail)
-//     console.log('la mail', userEmail ,'è stata salvata correttamente')
-//     // let userEmail = prompt('Aggiungi ulteriore email')
+if (savedEmail.includes(userEmail)) {
+    console.log('la mail è già presente')
+    let userEmail = prompt('inserisci un\'altra email')
+} else {
+    savedEmail.push(userEmail)
+    console.log('la mail', userEmail ,'è stata salvata correttamente')
+    // let userEmail = prompt('Aggiungi ulteriore email')
 
-// }
-
-// METODO CON FOR
-let i= userEmail
-for(i=0; i<savedEmail.length; i=i++){
-    if(savedEmail[i] == userEmail){
-        console.log('la mail è già presente')
-    } else{
-        savedEmail.push(userEmail)
-        console.log('la mail', userEmail ,'è stata salvata correttamente')
-    }
 }
+
+// // METODO CON FOR
+// let i= userEmail
+// for(i=0; i<savedEmail.length; i=i++){
+//     if(savedEmail[i] == userEmail){
+//         console.log('la mail è già presente')
+//     } else{
+//         savedEmail.push(userEmail)
+//         console.log('la mail', userEmail ,'è stata salvata correttamente')
+//     }
+// }
