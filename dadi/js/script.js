8433fadfb8897c91120ed3727189fe62f7f903bd
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-10-26 17:09:05 +0200

    Exercise completed Dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,22 +1,30 @@
 // recupero variabili
-const numUser=document.querySelector('.user-num')
-const numPc= document.querySelector('.pc-num')
-const numOutput= document.querySelector('.message')
+const numUser=document.querySelector('#user-num')
+const numPc= document.querySelector('#pc-num')
+const numOutput= document.querySelector('#message')
+const btnPlay = document.querySelector('#btn-Play')
+const btnReset = document.querySelector('#btn-Reset')
 
 
-// numUser= Math.floor((Math.random() * 6 ) + 1)
-// numPc=Math.floor((Math.random() * 6 ) + 1)
+btnPlay.addEventListener('click', function(){
 
-if (numUser>numPc) {
-    numUser.innerHTML= Math.floor((Math.random() * 6 ) + 1)
-    numPc.innerHTML=Math.floor((Math.random() * 6 ) + 1)
-    // numUser.innerHTML=(numUser)
-    // numPc.innerHTML=numPc
-    numOutput.innerHTML='hai vinto'
+    const userNum =Math.floor((Math.random() * 6 ) + 1)
+    const pcNum =Math.floor((Math.random() * 6 ) + 1)
+    numUser.innerHTML= userNum
+    numPc.innerHTML= pcNum
     
-} else if(numPc>numUser) {
-    numOutput.innerHTML='ha vinto il pc'
-    
-} else{
-    numOutput.innerHTML='pareggio'
-}
+    if (userNum > pcNum) {
+        numOutput.innerHTML='hai vinto'
+    }  else if (pcNum > userNum){
+        numOutput.innerHTML='hai perso'
+    } else{
+        numOutput.innerHTML='pareggio'
+    }
+
+})
+
+btnReset.addEventListener('click', function () {
+	numUser.innerHTML = '';
+	numPc.innerHTML = '';
+	numOutput.innerHTML = '';
+})
