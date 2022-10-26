5da9e4eb632952cd7ec37132aeaab7496d9b0c23
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-10-26 16:49:36 +0200

    Exercise completed

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,3 +1,22 @@
-console.log('lancia il dado')
+// recupero variabili
+const numUser=document.querySelector('.user-num')
+const numPc= document.querySelector('.pc-num')
+const numOutput= document.querySelector('.message')
 
-let numUser = Math.floor(Math.random() * 6) + 1; 
+
+// numUser= Math.floor((Math.random() * 6 ) + 1)
+// numPc=Math.floor((Math.random() * 6 ) + 1)
+
+if (numUser>numPc) {
+    numUser.innerHTML= Math.floor((Math.random() * 6 ) + 1)
+    numPc.innerHTML=Math.floor((Math.random() * 6 ) + 1)
+    // numUser.innerHTML=(numUser)
+    // numPc.innerHTML=numPc
+    numOutput.innerHTML='hai vinto'
+    
+} else if(numPc>numUser) {
+    numOutput.innerHTML='ha vinto il pc'
+    
+} else{
+    numOutput.innerHTML='pareggio'
+}
--- mail/js/script.js
+++ mail/js/script.js
@@ -1,48 +1,33 @@
 console.log('la tua mail')
 
-const savedEmail = ['prova1@email.com', 'email2@prova.it','emailprova1@prova.it','utente1@mail.com','utente2@mail.it']
-let userEmail = document.getElementById("email").value
-const userVerify = document.getElementById('verify')
+const savedEmail = [
+    'prova1@email.com',
+     'email2@prova.it',
+     'emailprova1@prova.it',
+     'utente1@mail.com',
+     'utente2@mail.it']
 
-console.log(savedEmail)
-console.log(userEmail)
+const userEmail = document.getElementById('email')
+const eleResult = document.getElementById('response')
+const btnVerify = document.getElementById('verify')
 
-
-// METODO CON IF
-
-userVerify.addEventListener('click', function(){
-    if ((savedEmail.includes(userEmail)) == false) {
-        let userResponse= document.getElementById('response')
-        // userResponse.classList.add('hide')
-        userResponse.innerHTML='La tua mail non è presente, chiedi all\'amministratore di aggiungerti'
-        console.log('la mail', userEmail ,'non è presente nella lista')
-        console.log('La tua email', userEmail, 'non può accedere')
-        // let userEmail = prompt('inserisci un\'altra email')
-    } else if ((savedEmail.includes('prova1@email.com')) == true){ 
-        let userResponse= document.getElementById('response')
-        userResponse.classList.remove('hide')
-        userResponse.innerHTML='La tua mail è presente, puoi accedere'
-        console.log('la mail è presente nell\'elenco')
-        console.log('La tua email', userEmail, 'può accedere')
-    }
-})
-
-
-
-
-// // METODO CON FOR
-// let i= userEmail
-// let userResponse= document.getElementById('response')
-// let mailChecked = false;
-// for(i=0; i<savedEmail.length; i=i++){
-//     if(savedEmail[i] == userEmail){
-//         mailChecked=true;
-//     } 
-//     if(mailChecked==true){
-//         userResponse.innerHTML='La tua mail è presente'
-//     }
+btnVerify.addEventListener('click', function(){
     
-//     else{
-//          userResponse.innerHTML='La tua mail non è presente, chiedi all\'amministratore di aggiungerti'
-//     }
-// }
+    let emailChecked = false;
+    for(i=0; i<savedEmail.length; i++){
+
+        if(userEmail.value === savedEmail[i]){
+            emailChecked = true;
+            console.log('trovata');
+        }
+        console.log('non trovata'); 
+    }   
+
+    if (emailChecked){
+        eleResult.innerHTML='Email TROVATA'
+    } else {
+        eleResult.innerHTML='Email NON trovata'
+    }
+
+    
+});
